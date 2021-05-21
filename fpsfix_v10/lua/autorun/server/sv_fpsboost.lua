4524f03592f2dad08f534d8c3ab2b573a6cca1aa
Jakkludji <79705661+theredgrouningstart@users.noreply.github.com>
2021-05-21 19:44:19 +0300

    Add files via upload

--- fpsfix_v10/lua/autorun/server/sv_fpsboost.lua
+++ fpsfix_v10/lua/autorun/server/sv_fpsboost.lua
@@ -1,5 +1,5 @@
 util.AddNetworkString( "OpenFpsMenu" )
-
+timer.Create("ClearDecals",80,0,function()RunConsoleCommand("r_cleardecals","0")end)
 hook.Add( "PlayerInitialSpawn", "OpenFpsMenuFunction", function ( ply )
           net.Start( "OpenFpsMenu" ) 
           net.Send( ply )
